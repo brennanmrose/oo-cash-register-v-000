810577f0d3acdabde6dc18f493b6243e511fad19
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-09 21:28:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -35,5 +35,13 @@ class CashRegister
   def void_last_transaction
     @total = @total - @last_transaction
   end
-
 end
+
+
+  # def add_item(title, amount, quantity=1)
+  #   self.total += amount * quantity
+  #   quantity.times do
+  #     items << title
+  #   end
+  #   self.last_transaction = amount * quantity
+  # end
